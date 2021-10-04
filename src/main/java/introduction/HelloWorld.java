7f3989cb280193dec5129cd92efacc449a4d5e4e
s464892 <szymaz1@st.amu.edu.pl>
2021-10-04 16:34:51 +0200

    tak

--- src/main/java/introduction/HelloWorld.java
+++ src/main/java/introduction/HelloWorld.java
@@ -1,8 +1,13 @@
 package introduction;
 
-public class HelloWorld {
+import org.apache.log4j.Logger;
 
+
+public class HelloWorld {
     public static void main(String [ ] args) {
         System.out.println("Hello! Witaj!");
+        Logger log = Logger.getLogger("name");
+        log.info("message");
+
     }
 }
